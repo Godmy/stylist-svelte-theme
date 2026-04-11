0cb772eb26cea28d81f1dd33905233c420698833
Lionfish <PavarenkinDA@gmail.com>
2026-04-11 23:03:09 +0300

    fix audit errors - green

---
+++ const/icon/index.ts
@@ -0,0 +1,8 @@
+export const TOKEN_THEME_ICON = [
+	'dark-mode',
+	'light-mode',
+	'styles',
+	'stylist-organism',
+	'themes',
+	'tokens'
+] as const;
--- type/enum/icon/index.ts
+++ type/enum/icon/index.ts
@@ -1,3 +1,3 @@
-import { TOKEN_THEME_ICON } from '$stylist/theme/const/enum/icon';
+import { TOKEN_THEME_ICON } from '$stylist/theme/const/icon';
 
 export type TokenThemeIcon = (typeof TOKEN_THEME_ICON)[number];
